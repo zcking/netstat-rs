a288d9c8282a24cdd5c0dbf7c0735483cf2b38de
ivxvm <ivxvm@protonmail.com>
2018-09-25 22:55:18 +0300

    Implement get_extended_tcp_table

--- src/main.rs
+++ src/main.rs
@@ -23,12 +23,29 @@ impl AddressFamily {
 }
 
 #[derive(Copy, Clone, Debug)]
-pub struct BindingInfo {
-    pub protocol: Protocol,
-    pub ip: IpAddr,
-    pub scope: Option<u32>,
-    pub port: u16,
+pub struct TcpBindingInfo {
+    pub local_addr: IpAddr,
+    pub local_scope: Option<u32>,
+    pub local_port: u16,
+    pub remote_addr: IpAddr,
+    pub remote_scope: Option<u32>,
+    pub remote_port: u16,
+    pub state: u32,
     pub pid: u32,
+}
+
+#[derive(Copy, Clone, Debug)]
+pub struct UdpBindingInfo {
+    pub local_addr: IpAddr,
+    pub local_scope: Option<u32>,
+    pub local_port: u16,
+    pub pid: u32,
+}
+
+#[derive(Copy, Clone, Debug)]
+pub enum Binding {
+    TcpBinding(TcpBindingInfo),
+    UdpBinding(UdpBindingInfo),
 }
 
 #[derive(Copy, Clone, Debug)]
@@ -43,9 +60,93 @@ pub struct Error {
     pub error_type: ErrorType,
 }
 
+fn get_extended_tcp_table(
+    address_family: AddressFamily,
+    bindings: &mut Vec<Binding>,
+) -> Result<(), Error> {
+    unsafe {
+        let af_ulong = address_family.as_ulong();
+        let mut buffer_size: ffi::DWORD = 0;
+        let mut err_code = ffi::GetExtendedTcpTable(
+            std::ptr::null_mut(),
+            &mut buffer_size,
+            ffi::FALSE,
+            af_ulong,
+            ffi::TCP_TABLE_OWNER_PID_ALL,
+            0,
+        );
+        let mut buffer = Vec::<u8>::new();
+        let mut iterations = 0;
+        while err_code == ffi::ERROR_INSUFFICIENT_BUFFER {
+            buffer = Vec::<u8>::with_capacity(buffer_size as usize);
+            err_code = ffi::GetExtendedTcpTable(
+                buffer.as_mut_ptr() as ffi::PVOID,
+                &mut buffer_size,
+                ffi::FALSE,
+                af_ulong,
+                ffi::TCP_TABLE_OWNER_PID_ALL,
+                0,
+            );
+            iterations += 1;
+            if iterations > 100 {
+                return Result::Err(Error {
+                    method_name: "GetExtendedTcpTable",
+                    error_type: ErrorType::BufferInitializationError(iterations),
+                });
+            }
+        }
+        if err_code == ffi::NO_ERROR {
+            match address_family {
+                AddressFamily::AF_INET => {
+                    let table_ref = &*(buffer.as_ptr() as *const ffi::MIB_TCPTABLE_OWNER_PID);
+                    let rows_count = table_ref.rows_count as usize;
+                    let row_ptr = &table_ref.rows[0] as *const ffi::MIB_TCPROW_OWNER_PID;
+                    for i in 0..rows_count {
+                        let row = &*row_ptr.offset(i as isize);
+                        bindings.push(Binding::TcpBinding(TcpBindingInfo {
+                            local_addr: IpAddr::V4(Ipv4Addr::from(u32::from_be(row.local_addr))),
+                            local_scope: Option::None,
+                            local_port: u16::from_be(row.local_port as u16),
+                            remote_addr: IpAddr::V4(Ipv4Addr::from(u32::from_be(row.remote_addr))),
+                            remote_scope: Option::None,
+                            remote_port: u16::from_be(row.remote_port as u16),
+                            state: row.state,
+                            pid: row.owning_pid,
+                        }));
+                    }
+                }
+                AddressFamily::AF_INET6 => {
+                    let table_ref = &*(buffer.as_ptr() as *const ffi::MIB_TCP6TABLE_OWNER_PID);
+                    let rows_count = table_ref.rows_count as usize;
+                    let row_ptr = &table_ref.rows[0] as *const ffi::MIB_TCP6ROW_OWNER_PID;
+                    for i in 0..rows_count {
+                        let row = &*row_ptr.offset(i as isize);
+                        bindings.push(Binding::TcpBinding(TcpBindingInfo {
+                            local_addr: IpAddr::V6(Ipv6Addr::from(row.local_addr)),
+                            local_scope: Option::Some(row.local_scope_id),
+                            local_port: u16::from_be(row.local_port as u16),
+                            remote_addr: IpAddr::V6(Ipv6Addr::from(row.remote_addr)),
+                            remote_scope: Option::Some(row.remote_scope_id),
+                            remote_port: u16::from_be(row.remote_port as u16),
+                            state: row.state,
+                            pid: row.owning_pid,
+                        }));
+                    }
+                }
+            }
+            return Result::Ok(());
+        } else {
+            return Result::Err(Error {
+                method_name: "GetExtendedTcpTable",
+                error_type: ErrorType::ErrorWithCode(err_code),
+            });
+        }
+    }
+}
+
 fn get_extended_udp_table(
     address_family: AddressFamily,
-    bindings: &mut Vec<BindingInfo>,
+    bindings: &mut Vec<Binding>,
 ) -> Result<(), Error> {
     unsafe {
         let af_ulong = address_family.as_ulong();
@@ -86,13 +187,12 @@ fn get_extended_udp_table(
                     let row_ptr = &table_ref.rows[0] as *const ffi::MIB_UDPROW_OWNER_PID;
                     for i in 0..rows_count {
                         let row = &*row_ptr.offset(i as isize);
-                        bindings.push(BindingInfo {
-                            protocol: Protocol::UDP,
-                            ip: IpAddr::V4(Ipv4Addr::from(u32::from_be(row.local_addr))),
-                            scope: Option::None,
-                            port: u16::from_be(row.local_port as u16),
+                        bindings.push(Binding::UdpBinding(UdpBindingInfo {
+                            local_addr: IpAddr::V4(Ipv4Addr::from(u32::from_be(row.local_addr))),
+                            local_scope: Option::None,
+                            local_port: u16::from_be(row.local_port as u16),
                             pid: row.owning_pid,
-                        });
+                        }));
                     }
                 }
                 AddressFamily::AF_INET6 => {
@@ -101,13 +201,12 @@ fn get_extended_udp_table(
                     let row_ptr = &table_ref.rows[0] as *const ffi::MIB_UDP6ROW_OWNER_PID;
                     for i in 0..rows_count {
                         let row = &*row_ptr.offset(i as isize);
-                        bindings.push(BindingInfo {
-                            protocol: Protocol::UDP,
-                            ip: IpAddr::V6(Ipv6Addr::from(row.local_addr)),
-                            scope: Option::Some(row.local_scope_id),
-                            port: u16::from_be(row.local_port as u16),
+                        bindings.push(Binding::UdpBinding(UdpBindingInfo {
+                            local_addr: IpAddr::V6(Ipv6Addr::from(row.local_addr)),
+                            local_scope: Option::Some(row.local_scope_id),
+                            local_port: u16::from_be(row.local_port as u16),
                             pid: row.owning_pid,
-                        });
+                        }));
                     }
                 }
             }
@@ -122,12 +221,22 @@ fn get_extended_udp_table(
 }
 
 fn main() {
-    let mut bindings = Vec::<BindingInfo>::with_capacity(128);
-    get_extended_udp_table(AddressFamily::AF_INET6, &mut bindings).expect("Error!!!");
+    let mut bindings = Vec::<Binding>::with_capacity(128);
+    get_extended_tcp_table(AddressFamily::AF_INET, &mut bindings).expect("Error!!!");
+    get_extended_tcp_table(AddressFamily::AF_INET6, &mut bindings).expect("Error!!!");
+    get_extended_udp_table(AddressFamily::AF_INET, &mut bindings).expect("Error!!!");
     for binding in bindings {
-        println!(
-            "ip = {}, scope = {:?}, port = {}, pid = {}",
-            binding.ip, binding.scope, binding.port, binding.pid
-        );
+        match binding {
+            Binding::TcpBinding(binding) => println!(
+                "{}:{} -> {}:{}, state = {}, pid = {}",
+                binding.local_addr,
+                binding.local_port,
+                binding.remote_addr,
+                binding.remote_port,
+                binding.state,
+                binding.pid
+            ),
+            _ => {}
+        }
     }
 }
